7a15105acad190649aba846e12bf52cc99fcf112
nsharpe1 <nicholas.sharpe@keyin.com>
2024-12-10 10:29:17 -0330

    first commit

---
+++ index.js
@@ -0,0 +1,224 @@
+const express = require('express');
+const expressWs = require('express-ws');
+const path = require('path');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+
+const PORT = 3000;
+//TODO: Update this URI to match your own MongoDB setup
+const MONGO_URI = 'mongodb://localhost:27017/keyin_test';
+const app = express();
+expressWs(app);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(session({
+    secret: 'voting-app-secret',
+    resave: false,
+    saveUninitialized: false,
+}));
+let connectedClients = [];
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true },
+}); 
+
+const User = mongoose.model('User', userSchema);
+
+const pollSchema = new mongoose.Schema({
+    question: { type: String, required: true },
+    options: [
+        {
+            answer: { type: String, required: true },
+            votes: { type: Number, required: true }
+        }
+    ]
+})
+
+const Poll = mongoose.model('Poll', pollSchema);
+
+//Note: Not all routes you need are present here, some are missing and you'll need to add them yourself.
+
+app.ws('/ws', (socket, request) => {
+    connectedClients.push(socket);
+
+    socket.on('message', async (message) => {
+        const data = JSON.parse(message);
+        return console.log(data);
+    });
+
+    socket.on('close', async (message) => {
+        
+    });
+});
+
+app.get('/', async (request, response) => {
+    if (request.session.user?.id) {
+        return response.redirect('/dashboard');
+    }
+
+    const filter = {};
+    const allpolls = await Poll.find(filter);
+    const pollslength = allpolls.length;
+    console.log(pollslength);
+
+    response.render('index/unauthenticatedIndex', {pollsnumber: pollslength});
+});
+
+app.get('/login', async (request, response) => {
+    const errorMessage = request.query.error || null;
+    return response.render('login', { errorMessage });
+});
+
+app.post('/login', async (request, response) => {
+    const requestBody = request.body;
+
+    const user = await User.findOne({username: requestBody.username});
+    // console.log(user);
+
+    if (!user) {
+        console.error("Invalid credentials");
+        return response.redirect('/login?error=Invalid username');
+    }
+    const match = await bcrypt.compare(requestBody.password,user.password)
+    if (!match) {
+        console.error("Invalid credentials");
+        return response.redirect('/login?error=Invalid password');
+    } 
+    else {
+        request.session.userId = user._id;
+        return response.redirect('dashboard'); 
+    }
+});
+
+app.get('/signup', async (request, response) => {
+    if (request.session.userId) {
+        return response.redirect('/dashboard');
+    }
+
+    return response.render('signup', { errorMessage: null });
+});
+
+app.post("/signup", async (request, response) => {
+    const { username, password } = request.body;
+    const hashedpassword = await bcrypt.hash(password,10);
+    
+    const user1 = new User({ username: username, password: hashedpassword, role: "user" });
+    await user1.save();
+    return response.redirect('/');
+});
+
+app.get('/dashboard', async (request, response) => {
+    const filter = {};
+    const allpolls = await Poll.find(filter);
+
+    console.log(JSON.stringify(allpolls));
+    
+    if (!request.session.userId) {
+        return response.redirect('/');
+    }
+
+    //TODO: Fix the polls, this should contain all polls that are active. I'd recommend taking a look at the
+    // authenticatedIndex template to see how it expects polls to be represented
+    return response.render('index/authenticatedIndex', { polls: allpolls });
+});
+
+app.get('/profile', async (request, response) => {
+    return response.render('profile');
+});
+
+app.get('/createPoll', async (request, response) => {
+    // if (!request.session.user?.id) {
+        // return response.redirect('/');
+    // }
+
+    return response.render('createPoll')
+});
+
+// Poll creation
+app.post('/createPoll', async (request, response) => {
+    const { question, options } = request.body;
+    const formattedOptions = Object.values(options).map((option) => ({ answer: option, votes: 0 }));
+
+    const poll1 = new Poll({ question: question, options: formattedOptions });
+    await poll1.save();
+    const socket = new WebSocket('ws://localhost:3000/ws');
+    socket.send(
+        { question: question, options: formattedOptions }
+    );
+    return response.redirect('/');
+
+    const pollCreationError = onCreateNewPoll(question, formattedOptions);
+    //TODO: If an error occurs, what should we do?
+});
+
+app.post('/logout', (request, response) => {
+    request.session.destroy((error) => {
+        if (error) {
+            return response.status(500).send('Failed to log out.');
+        }
+        response.redirect('/');
+    });
+});
+
+mongoose.connect(MONGO_URI)
+    .then(() => app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`)))
+    .catch((err) => console.error('MongoDB connection error:', err));
+
+/**
+ * Handles creating a new poll, based on the data provided to the server
+ * 
+ * @param {string} question The question the poll is asking
+ * @param {[answer: string, votes: number]} pollOptions The various answers the poll allows and how many votes each answer should start with
+ * @returns {string?} An error message if an error occurs, or null if no error occurs.
+ */
+async function onCreateNewPoll(question, pollOptions) {
+    let newPoll = null;
+    try {
+         newPoll = new Poll({
+            question: question,
+            options: pollOptions
+        });
+        await newPoll.save()
+    }
+    catch (error) {
+        console.error(error);
+        return "Error creating the poll, please try again";
+    }
+
+    sendMessagetoCilents({
+       
+    })
+    //TODO: Tell all connected sockets that a new poll was added
+
+    return null;
+}
+
+async function sendMessagetoCilents(pollmessage) {
+    connectedClients.forEach((client) => {
+        client.send(JSON.stringify(pollmessage))
+    })
+}
+/**
+ * Handles processing a new vote on a poll
+ * 
+ * This function isn't necessary and should be removed if it's not used, but it's left as a hint to try and help give
+ * an idea of how you might want to handle incoming votes
+ * 
+ * @param {string} pollId The ID of the poll that was voted on
+ * @param {string} selectedOption Which option the user voted for
+ */
+async function onNewVote(pollId, selectedOption) {
+    try {
+        
+    }
+    catch (error) {
+        console.error('Error updating poll:', error);
+    }
+}
---
+++ public/frontend.js
@@ -0,0 +1,108 @@
+// Establish a WebSocket connection to the server
+const pollList = document.getElementById('poll-list');
+const socket = new WebSocket('ws://localhost:3000/ws');
+
+
+// Listen for messages from the server
+socket.addEventListener('message', (event) => {
+    const data = JSON.parse(event.data);
+    
+    //TODO: Handle the events from the socket
+});
+
+
+/**
+ * Handles adding a new poll to the page when one is received from the server
+ * 
+ * @param {*} data The data from the server (ideally containing the new poll's ID and it's corresponding questions)
+ */
+function onNewPollAdded(data) {
+    //TODO: Fix this to add the new poll to the page
+    
+    const pollContainer = document.getElementById('polls');
+    const newPoll = null;
+    pollContainer.appendChild(newPoll);
+
+    //TODO: Add event listeners to each vote button. This code might not work, it depends how you structure your polls on the poll page. However, it's left as an example 
+    //      as to what you might want to do to get clicking the vote options to actually communicate with the server
+    newPoll.querySelectorAll('.poll-form').forEach((pollForm) => {
+        pollForm.addEventListener('submit', onVoteClicked);
+    });
+}
+
+/**
+ * Handles updating the number of votes an option has when a new vote is recieved from the server
+ * 
+ * @param {*} data The data from the server (probably containing which poll was updated and the new vote values for that poll)
+ */
+function onIncomingVote(data) {
+    
+}
+
+/**
+ * Handles processing a user's vote when they click on an option to vote
+ * 
+ * @param {FormDataEvent} event The form event sent after the user clicks a poll option to "submit" the form
+ */
+function onVoteClicked(event) {
+    console.log("Vote clicked!")
+    //Note: This function only works if your structure for displaying polls on the page hasn't changed from the template. If you change the template, you'll likely need to change this too
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    const pollId = formData.get("poll-id");
+    const selectedOption = event.submitter.value;
+
+    const pollInput = document.getElementById('poll');
+    socket.send(
+        JSON.stringify(
+            { pollId: pollId, selectedOption: selectedOption }
+        )
+    );
+    
+    //TOOD: Tell the server the user voted
+}
+
+//Adds a listener to each existing poll to handle things when the user attempts to vote
+document.querySelectorAll('.poll-form').forEach((pollForm) => {
+    pollForm.addEventListener('submit', onVoteClicked);
+});
+
+
+
+
+
+//Connect socket
+//Add event listener to listen for new socket messages
+//Add event listener to handle sending the form data via the socket.
+
+const stockForm = document.getElementById('stock-form');
+
+socket.addEventListener('message', (event) => {
+    const message = event.data;
+    const json = JSON.parse(message);
+
+    for (const [stock, price] of Object.entries(json)) {
+       let stockListItem = document.getElementById(stock);
+       if (!stockListItem) {
+           stockListItem = document.createElement('li');
+           stockListItem.id = stock;
+           const stockList = document.getElementById('stocks');
+           stockList.appendChild(stockListItem);
+       }
+       stockListItem.innerHTML = `${stock}: <span>${price.toFixed(2)}</span>`
+    }
+});
+
+stockForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const stockInput = document.getElementById('stock');
+    socket.send(
+        JSON.stringify(
+            { stockName: stockInput.value }
+        )
+    );
+
+    stockInput.value = '';
+});
